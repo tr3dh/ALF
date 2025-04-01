d4f1442c8617c16f76afc4f9926ee415ac891611
tr3dh <timalrot@gmail.com>
2025-04-01 15:55:39 +0200

    Funktionsrümpfe aus dem StringProcessing Header in die entsprechende *.cpp ausgelagert

--- src/Drivers/__StringProcessing.cpp
+++ src/Drivers/__StringProcessing.cpp
@@ -2,6 +2,44 @@
 
 namespace string{
 
-    // später Deklarationen der __StringProcessing.h einfügen
-    
+    //
+    bool contains(const std::string& str, const std::string& subStr){
+        return str.find(subStr) != std::string::npos;
+    }
+
+    // Findet das erste Vorkommen eines Substrings in einem String
+    std::string::size_type findFirst(std::string_view str, std::string_view subStr) {
+        return str.find(subStr);
+    }
+
+    // Findet das letzte Vorkommen eines Substrings in einem String
+    std::string::size_type findLast(std::string_view str, std::string_view subStr) {
+        return str.rfind(subStr);
+    }
+
+    std::string trim(const std::string& str, const char& token) {
+        auto start = std::ranges::find_if_not(str, [&token](unsigned char c) {
+            return c == token;
+        });
+        auto end = std::ranges::find_if_not(str | std::views::reverse, [&token](unsigned char c) {
+            return c == token;
+        }).base();
+        return (start < end) ? std::string(start, end) : "";
+    }
+
+    template<typename T>
+    T convert(const std::string& str) {
+
+        T result;
+        auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), result);
+
+        if (ec != std::errc()) throw std::invalid_argument("Invalid number");
+        return result;
+    }
+
+    std::vector<std::string> split(const std::string& str, const char& token){
+        
+        //
+        return str | std::views::split(token) | std::ranges::to<std::vector<std::string>>();
+    }
 }
--- src/Drivers/__StringProcessing.h
+++ src/Drivers/__StringProcessing.h
@@ -9,43 +9,18 @@
 namespace string{
 
     //
-    inline bool contains(const std::string& str, const std::string& subStr){
-        return str.find(subStr) != std::string::npos;
-    }
+    bool contains(const std::string& str, const std::string& subStr);
 
     // Findet das erste Vorkommen eines Substrings in einem String
-    inline std::string::size_type findFirst(std::string_view str, std::string_view subStr) {
-        return str.find(subStr);
-    }
+    std::string::size_type findFirst(std::string_view str, std::string_view subStr);
 
     // Findet das letzte Vorkommen eines Substrings in einem String
-    inline std::string::size_type findLast(std::string_view str, std::string_view subStr) {
-        return str.rfind(subStr);
-    }
+    std::string::size_type findLast(std::string_view str, std::string_view subStr);
 
-    inline std::string trim(const std::string& str, const char& token = ' ') {
-        auto start = std::ranges::find_if_not(str, [&token](unsigned char c) {
-            return c == token;
-        });
-        auto end = std::ranges::find_if_not(str | std::views::reverse, [&token](unsigned char c) {
-            return c == token;
-        }).base();
-        return (start < end) ? std::string(start, end) : "";
-    }
+    std::string trim(const std::string& str, const char& token = ' ');
 
     template<typename T>
-    T convert(const std::string& str) {
+    T convert(const std::string& str);
 
-        T result;
-        auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), result);
-
-        if (ec != std::errc()) throw std::invalid_argument("Invalid number");
-        return result;
-    }
-
-    inline std::vector<std::string> split(const std::string& str, const char& token = ' '){
-        
-        //
-        return str | std::views::split(token) | std::ranges::to<std::vector<std::string>>();
-    }
+    std::vector<std::string> split(const std::string& str, const char& token = ' ');
 }
